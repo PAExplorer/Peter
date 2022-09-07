cfe1be2b0f2b9b11da23cd1e9f8ac178a16ad763
MrBicycle <52579834+MrBicycle@users.noreply.github.com>
2022-09-07 17:09:00 -0400

    added a lot of elements
    Working doors, music and enemy kills now

--- peter.py
+++ peter.py
@@ -3,7 +3,7 @@
 #   Sep 2022                                        #
 
 #Initialize Modules
-import pygame, sys, random
+import pygame, sys, random, time
 
 #Define some variables
 scale = 3
@@ -24,6 +24,13 @@ currentFood = 0
 level = 0
 clockTickSet = int(30 + (scale * 30))
 paused = False
+doorsOpen = False
+lives = 3
+waitTime = 1
+scatterTime = 1000
+enemyTargX = 0
+enemyTargY = 0
+trackList = ["sound/Leap.wav","sound/Turns.wav","sound/Spooky.mp3"]
 
 #Define some levels as strings
 level1Template =  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
@@ -56,40 +63,40 @@ level1Template += "xwww.ww.ww.wwwwwwww.ww.ww.www"
 level1Template += "xw......ww....ww....ww......w"
 level1Template += "xw.wwwwwwwwww.ww.wwwwwwwwww.w"
 level1Template += "xw.wwwwwwwwww.ww.wwwwwwwwww.w"
-level1Template += "xw..........................w"
+level1Template += "xw............ss............w"
 level1Template += "xwwwwwwwwwwwwwwwwwwwwwwwwwwww"
 
 level2Template = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 level2Template += "xwwwwwwwwwwwwwwwwwwwwwwwwwwww"
-level2Template += "xw....wwwwwwwwwwwwwwwwww....w"
-level2Template += "xw.ww.........ww.........ww.w"
+level2Template += "xws.......wwwwwwwwww.......sw"
+level2Template += "xw.wwwwww.....ww.....wwwwww.w"
 level2Template += "xw.wwwwwwwwww.ww.wwwwwwwwww.w"
 level2Template += "xw.wwwwwwwwww.ww.wwwwwwwwww.w"
 level2Template += "xw......ww..........ww......w"
 level2Template += "xw.wwww.ww.wwwwwwww.ww.wwww.w"
 level2Template += "xw.wwww.ww.wwwwwwww.ww.wwww.w"
 level2Template += "xw............ww............w"
-level2Template += "xwwwwww.wwwwwxwwxwwwww.wwwwww"
+level2Template += "xwwwwww.wwwwwgwwgwwwww.wwwwww"
 level2Template += "xxxxwww.wwwwwxwwxwwwww.wwwxxx"
 level2Template += "xxxxw...ww..........ww...wxxx"
-level2Template += "xxxxw.wwww.wwwxxwww.wwww.wxxx"
+level2Template += "xxxxw.wwww.wwwggwww.wwww.wxxx"
 level2Template += "xxxxw.wxxw.wxxxxxxw.wxxw.wxxx"
 level2Template += "xxxxw.wxxw.wxxxxxxw.wxxw.wxxx"
 level2Template += "xxxxw.wxxw.wexxxxew.wxxw.wxxx"
-level2Template += "xxxxw.wwww.wwwxxwww.wwww.wxxx"
-level2Template += "xxxxw...ww..........ww...wxxx"
-level2Template += "xxxxwww.wwwwwxwwxwwwww.wwwxxx"
-level2Template += "xwwwwww.wwwwwxwwxwwwww.wwwwww"
-level2Template += "xw............ww............w"
+level2Template += "xwwww.wwww.wwwggwww.wwww.wwww"
+level2Template += "xw......ww..........ww......w"
+level2Template += "xw.wwww.wwwwwxwwxwwwww.wwww.w"
+level2Template += "xw.wwww.wwwwwgwwgwwwww.wwww.w"
+level2Template += "xw..........................w"
 level2Template += "xw.wwww.wwwww.ww.wwwww.wwww.w"
 level2Template += "xw.wwww.wwwww.ww.wwwww.wwww.w"
 level2Template += "xw...ww.ww....xx....ww.ww...w"
 level2Template += "xwww.ww.ww.wwwwwwww.ww.ww.www"
 level2Template += "xwww.ww.ww.wwwwwwww.ww.ww.www"
 level2Template += "xw............ww............w"
-level2Template += "xw.wwwwwwwwww.ww.wwwwwwwwww.w"
-level2Template += "xw.wwwwwwwwww.ww.wwwwwwwwww.w"
-level2Template += "xw..........................w"
+level2Template += "xw.wwww.wwwww.ww.wwwww.wwww.w"
+level2Template += "xw.wwww.wwwww.ww.wwwww.wwww.w"
+level2Template += "xws........................sw"
 level2Template += "xwwwwwwwwwwwwwwwwwwwwwwwwwwww"
 
 level3Template = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
@@ -188,25 +195,25 @@ class Enemy(pygame.sprite.Sprite): #Enemy(cellPx, cellPx, xP * cellPx, yP * cell
         self.eGridX = 0
         self.eGridY = 0
     def update(self):
+        blockingHit = pygame.sprite.spritecollide(man, enemyGroup, False)
+        for block in blockingHit:
+            removeLife(1)
+        
         if int(self.ePosX % cellPx) == 0 and int(self.ePosY % cellPx) == 0:
             #Code to find direction should go here
             self.eGridX = (self.ePosX)/cellPx
             self.eGridY = (self.ePosY)/cellPx
             
-            if self.ePosX > manX and levelCharCheck(self.eGridX, self.eGridY, 0, "w") == False and self.eDirection != 1:
-                print("A")
+            if self.ePosX > enemyTargX and levelCollideCheck(self.eGridX, self.eGridY, 0) == False and self.eDirection != 1:
                 self.eDirection = 0
-            elif self.ePosX < manX and levelCharCheck(self.eGridX, self.eGridY, 1, "w") == False and self.eDirection != 0:
-                print("B")
+            elif self.ePosX < enemyTargX and levelCollideCheck(self.eGridX, self.eGridY, 1) == False and self.eDirection != 0:
                 self.eDirection = 1
-            elif self.ePosY > manY and levelCharCheck(self.eGridX, self.eGridY, 2, "w") == False and self.eDirection != 3:
-                print("C")
+            elif self.ePosY > enemyTargY and levelCollideCheck(self.eGridX, self.eGridY, 2) == False and self.eDirection != 3:
                 self.eDirection = 2
-            elif self.ePosY < manY and levelCharCheck(self.eGridX, self.eGridY, 3, "w") == False and self.eDirection != 2:
-                print("D")
+            elif self.ePosY < enemyTargY and levelCollideCheck(self.eGridX, self.eGridY, 3) == False and self.eDirection != 2:
                 self.eDirection = 3
             else:
-                while levelCharCheck(self.eGridX, self.eGridY, self.eDirection, "w") == True:
+                while levelCollideCheck(self.eGridX, self.eGridY, self.eDirection) == True:
                     self.eDirection = random.randint(0,3)
             
             
@@ -223,21 +230,66 @@ class Enemy(pygame.sprite.Sprite): #Enemy(cellPx, cellPx, xP * cellPx, yP * cell
                 print("Can't move!")
         self.rect.topleft = [self.ePosX, self.ePosY]
 
-def levelCharCheck(currentX, currentY, askDirection, askChar): #takes Coords in grid space not pixel space
+class Door(pygame.sprite.Sprite):
+    def __init__(self, width, height, pos_x, pos_y):
+        super().__init__()
+        self.image = pygame.Surface([width, height])
+        self.image = pygame.image.load("img/Door.png")
+        self.image = pygame.transform.scale(self.image, (cellPx, cellPx))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = [pos_x, pos_y]
+        self.xLoc = pos_x
+        self.yLoc = pos_y
+    def update(self):
+        if doorsOpen == True:
+            self.image = pygame.image.load("img/DoorOpen.png")
+            self.image = pygame.transform.scale(self.image, (cellPx, cellPx))
+            self.rect = self.image.get_rect()
+            self.rect.topleft = [self.xLoc, self.yLoc]
+
+class Switch(pygame.sprite.Sprite):
+    def __init__(self, width, height, pos_x, pos_y):
+        super().__init__()
+        self.image = pygame.Surface([width, height])
+        self.image = pygame.image.load("img/Switch.png")
+        self.image = pygame.transform.scale(self.image, (cellPx, cellPx))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = [pos_x, pos_y]
+        self.xLoc = pos_x
+        self.yLoc = pos_y
+    def update(self):
+        blockingHit = pygame.sprite.spritecollide(man, switchGroup, False)
+        for block in blockingHit:
+            openDoors(True)
+        if doorsOpen == True:
+            self.image = pygame.image.load("img/SwitchFlip.png")
+            self.image = pygame.transform.scale(self.image, (cellPx, cellPx))
+            self.rect = self.image.get_rect()
+            self.rect.topleft = [self.xLoc, self.yLoc]
+
+def levelCollideCheck(currentX, currentY, askDirection): #takes Coords in grid space not pixel space
     currentX = int(currentX)
     currentY = int(currentY)
     match askDirection: #Check if we can change our existing movement
         case 0: #Left
-            if levelList[level][currentY * cellX  + clamp(currentX - 1, 0 , cellX)] == askChar:
+            if levelList[level][currentY * cellX  + clamp(currentX - 1, 0 , cellX)] == "w":
+                return True
+            elif doorsOpen == False and levelList[level][currentY * cellX  + clamp(currentX - 1, 0 , cellX)] == "g":
                 return True
         case 1: #Right
-            if levelList[level][currentY * cellX  + clamp(currentX + 1, 0 , cellX)] == askChar:
+            if levelList[level][currentY * cellX  + clamp(currentX + 1, 0 , cellX)] == "w":
+                return True
+            elif doorsOpen == False and levelList[level][currentY * cellX  + clamp(currentX + 1, 0 , cellX)] == "g":
                 return True
         case 2: #Up
-            if levelList[level][clamp((currentY - 1) * cellX, 0, cellY*cellX) + currentX] == askChar:
+            if levelList[level][clamp((currentY - 1) * cellX, 0, cellY*cellX) + currentX] == "w":
+                return True
+            elif doorsOpen == False and levelList[level][clamp((currentY - 1) * cellX, 0, cellY*cellX) + currentX] == "g":
                 return True
         case 3: #Down
-            if levelList[level][ clamp((currentY + 1) * cellX, 0, cellY*cellX) + currentX ] == askChar:
+            if levelList[level][ clamp((currentY + 1) * cellX, 0, cellY*cellX) + currentX ] == "w":
+                return True
+            elif doorsOpen == False and levelList[level][ clamp((currentY + 1) * cellX, 0, cellY*cellX) + currentX ] == "g":
                 return True
     return False
 
@@ -267,33 +319,33 @@ def ManGridCheckpoint(locX, locY): #location X and location Y of the player char
         
         #First Check if we're on a food
         newFood.update()
-        
+        newSwitch.update()
         match setDir:#Check if we can change our existing movement
             case 0: #Left
-                if levelCharCheck(gridX, gridY, 0, "w") == False:
+                if levelCollideCheck(gridX, gridY, 0) == False:
                     peterDir = setDir
             case 1: #Right
-                if levelCharCheck(gridX, gridY, 1, "w") == False:
+                if levelCollideCheck(gridX, gridY, 1) == False:
                     peterDir = setDir
             case 2: #Up
-                if levelCharCheck(gridX, gridY, 2, "w") == False:
+                if levelCollideCheck(gridX, gridY, 2) == False:
                     peterDir = setDir
             case 3: #Down
-                if levelCharCheck(gridX, gridY, 3, "w") == False :
+                if levelCollideCheck(gridX, gridY, 3) == False :
                     peterDir = setDir
         
         match peterDir:#Check if we can change our existing movement
             case 0: #Left
-                if levelCharCheck(gridX, gridY, 0, "w") == True:
+                if levelCollideCheck(gridX, gridY, 0) == True:
                     bCanContinue = False
             case 1: #Right
-                if levelCharCheck(gridX, gridY, 1, "w") == True:
+                if levelCollideCheck(gridX, gridY, 1) == True:
                     bCanContinue = False
             case 2: #Up
-                if levelCharCheck(gridX, gridY, 2, "w") == True:
+                if levelCollideCheck(gridX, gridY, 2) == True:
                     bCanContinue = False
             case 3: #Down
-                if levelCharCheck(gridX, gridY, 3, "w") == True:
+                if levelCollideCheck(gridX, gridY, 3) == True:
                     bCanContinue = False
 
 def addScore(amount):
@@ -308,6 +360,39 @@ def addLevel(amount):
     global level
     level = (level + amount) % len(levelList)
 
+def openDoors(boolIn):
+    global doorsOpen
+    doorsOpen = boolIn
+    print(doorsOpen)
+    #pygame.sprite.Group.empty(switchGroup)
+    #pygame.sprite.Group.empty(doorGroup)
+
+def removeLife(amount):
+    global lives
+    lives -= amount
+    setupLevel(levelList[level])
+    setupEnemy(levelList[level])
+    setupDoors(levelList[level])
+    setupSwitch(levelList[level])
+    setManLocation(manStartX, manStartY)
+    setWaitTime(3)
+    pygame.mixer.music.stop()
+
+def setManLocation(pos_x, pos_y):
+    global manX
+    global manY
+    manX = pos_x
+    manY = pos_y
+
+def setEnemyTarg(pos_x, pos_y):
+    global enemyTargX
+    global enemyTargY
+    enemyTargX = pos_x
+    enemyTargY = pos_y
+
+def setWaitTime(amount):
+    global waitTime
+    waitTime = amount
 #General Setup and backdrop
 pygame.init()
 clock = pygame.time.Clock()
@@ -318,21 +403,37 @@ screen.fill((14,14,14))
 wallGroup = pygame.sprite.Group()
 def setupLevel(level):
     global wallGroup
+    pygame.mixer.music.load(trackList[random.randint(0,len(trackList) - 1)])
+    pygame.mixer.music.play(-1)
     pygame.sprite.Group.empty(wallGroup)
     xP = 0
     yP = 0
     setColor = (255,255,255)
+    wallColor = (random.randint(14,123),random.randint(14,123),random.randint(14,123))
     for xP in range(0, cellX):
         for yP in range(0, cellY):
             if level[(yP * cellX) + xP] == "w":
-                setColor = (46,123,92)
-            elif level[(yP * cellX) + xP] == "g":
-                setColor = (46,123,46)
+                setColor = (wallColor)
             else:
                 setColor = (14,14,14)
             newWall = Wall(cellPx, cellPx, xP * cellPx, yP * cellPx, setColor)
             wallGroup.add(newWall)
 
+#Set up Doors
+doorGroup = pygame.sprite.Group()
+def setupDoors(level):
+    global doorGroup
+    global newDoor
+    openDoors(False)
+    pygame.sprite.Group.empty(doorGroup)
+    xP=0
+    yP=0
+    for xP in range(0, cellX):
+        for yP in range(0, cellY):
+            if level[(yP * cellX) + xP] == "g":
+                newDoor = Door(cellPx, cellPx, xP * cellPx, yP * cellPx)
+                doorGroup.add(newDoor)
+                
 #set up pellet group
 foodGroup = pygame.sprite.Group()
 def setupFood(level):
@@ -348,6 +449,21 @@ def setupFood(level):
                 newFood = Food(cellPx, cellPx, xP * cellPx, yP * cellPx, setColor)
                 foodGroup.add(newFood)
                 removeFood(-1)
+
+#Set up switches
+switchGroup = pygame.sprite.Group()
+def setupSwitch(level):
+    global switchGroup
+    global newSwitch
+    pygame.sprite.Group.empty(switchGroup)
+    xP=0
+    yP=0
+    for xP in range(0, cellX):
+        for yP in range(0, cellY):
+            if level[(yP * cellX) + xP] == "s":
+                newSwitch = Switch(cellPx, cellPx, xP * cellPx, yP * cellPx)
+                switchGroup.add(newSwitch)
+
 
 #set up enemy group
 enemyGroup = pygame.sprite.Group()
@@ -372,12 +488,20 @@ manGroup.add(man)
 font = pygame.font.SysFont(None, 35 + scale*3)
 
 
+
+
+
+
+
 #Prelevel Setup
 setupLevel(levelList[0])
 setupFood(levelList[0])
 setupEnemy(levelList[0])
+setupDoors(levelList[0])
+setupSwitch(levelList[0])
+
 #Primary Game Loop
-while True:
+while lives > 0:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -407,11 +531,22 @@ while True:
                 setupLevel(levelList[level])
                 setupFood(levelList[level])
                 setupEnemy(levelList[level])
+                setupSwitch(levelList[level])
+                setupDoors(levelList[level])
                 manX = manStartX
                 manY = manStartY
                 print("Next Level")
     ManGridCheckpoint(manX, manY) #Primary Movement for player character
     
+    
+    #Toggle Scatter Mode for enemies
+    if scatterTime > 300:
+        setEnemyTarg(manX, manY)
+    elif scatterTime <= 300:
+        setEnemyTarg(random.randint(0, cellX) * cellPx,random.randint(0, cellY) * cellPx)
+        if scatterTime <= 0:
+            scatterTime = 1000
+    print(scatterTime)
     if bCanContinue == True and not paused:
         match peterDir:
             case 0: #Left
@@ -429,6 +564,8 @@ while True:
         setupLevel(levelList[level])
         setupFood(levelList[level])
         setupEnemy(levelList[level])
+        setupDoors(levelList[level])
+        setupSwitch(levelList[level])
         manX = manStartX
         manY = manStartY
         
@@ -438,16 +575,26 @@ while True:
     #screen.blit(background, (0,0))
     wallGroup.draw(screen)
     foodGroup.draw(screen)
+    switchGroup.draw(screen)
+    doorGroup.draw(screen)
     #Draw Enemies
     enemyGroup.draw(screen)
     #Draw Peter
     manGroup.draw(screen)
     #Draw Score
-    scoreBoard = font.render("Score: " + str(score), True, (123,123,123))
-    screen.blit(scoreBoard, (screenWidth * .75, screenHeight * .05))
+    screenText = font.render("Score: " + str(score), True, (123,123,123))
+    screen.blit(screenText, (screenWidth * .75, screenHeight * .05))
+    screenText = font.render("Lives: " + str(lives), True, (123,123,123))
+    screen.blit(screenText, (screenWidth * .75, screenHeight * .1))
     #Update
     if not paused:
         man.update()
         enemyGroup.update()
+        doorGroup.update()
+        switchGroup.update()
+        scatterTime -= 1
+    #Wait if needed
+    time.sleep(waitTime)
+    setWaitTime(0)
     clock.tick(clockTickSet)
 
